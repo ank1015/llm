946d6f744fa1c57bbc47a060fbaccf8ca824d61a
ananya <akblockchain15@gmail.com>
2026-04-04 13:21:47 +0530

    feat(core): add image runtime registry

---
+++ packages/core/src/images/index.ts
@@ -0,0 +1,10 @@
+export { generateImage } from './generate.js';
+export {
+  calculateImageCost,
+  getImageModel,
+  getImageModels,
+  getImageProviders,
+} from './models/index.js';
+export { registerImageProvider } from './providers/index.js';
+
+export type { ImageProviderRegistration } from './providers/index.js';
